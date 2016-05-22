a63b3de82d3c1aed1b54bf838d7784878d3e27be
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-05-21 21:27:52 -0300

    Changes DB structure

--- src/app/src/main/java/net/nkbits/epubcomic/provider/DBOpenHelper.java
+++ src/app/src/main/java/net/nkbits/epubcomic/provider/DBOpenHelper.java
@@ -21,25 +21,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBOpenHelper extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "acv";
+    private static final String DATABASE_NAME = "eComic";
     private static final int DATABASE_VERSION = 1;
     public static final String FILES_TABLE = "files";
+    public static final String ID_COLUMN = "id";
     public static final String PATH_COLUMN = "path";
-    public static final String BOOKMARK_COLUMN = "bookmark";
     public static final String READ_COLUMN = "read";
-    public static final String OPENED_COLUMN = "opened";
-    public static final String VIEWS_COLUMN = "views";
+    public static final String ADDED_COLUMN = "added";
+    public static final String LAST_READ = "last_read";
     public static final String FAVORITE_COLUMN = "favorite";
+    public static final String TYPE_COLUMN = "type";
 
     private static final String FILES_TABLE_CREATE =
-    		"CREATE TABLE " + FILES_TABLE +" ( " +
-    		"id INTEGER PRIMARY KEY, " +
-    		PATH_COLUMN + " TEXT NOT NULL, " +
-    		BOOKMARK_COLUMN + " INTEGER NOT NULL, " +
-    		READ_COLUMN + " BOOLEAN NOT NULL, " +
-    		OPENED_COLUMN + " DATE NOT NULL," +
-    		VIEWS_COLUMN + " INTEGER NOT NULL, " +
-    		FAVORITE_COLUMN + " INTEGER NOT NULL)";
+            "CREATE TABLE " + FILES_TABLE +" ( " +
+                    ID_COLUMN + " INTEGER PRIMARY KEY AUTO INCREMENT, " +
+                    PATH_COLUMN + " TEXT NOT NULL, " +
+                    READ_COLUMN + " BOOLEAN NOT NULL DEFAULT 0, " +
+                    ADDED_COLUMN + " DATE NOT NULL, " +
+                    LAST_READ + " DATE NULL," +
+                    TYPE_COLUMN + " BOOLEAN NOT NULL" +
+                    FAVORITE_COLUMN + " BOOLEAN NOT NULL DEFAULT 0)";
 
     public DBOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
